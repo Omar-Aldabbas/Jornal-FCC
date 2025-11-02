150a16c1be01b090b8aca7492d76b397941fbff3
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-11-02 07:48:47 -0800

    Initial Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import { Globe2 } from "lucide-react"
 
 export const Header = () => {
 
     return (
         <header>
-            
+           <span><Globe2 size={40} /></span> <h1>my travle jornal.</h1>
         </header>
     )
 }
